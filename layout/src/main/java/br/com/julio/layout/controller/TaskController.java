8b16a8225132d0ba5bd8f604c38ced6e82ccd53b
Julio César <juliocesarcs2004@gmail.com>
2022-08-11 11:21:06 -0300

    Incluídos botões, navbar e tela de listagem

--- layout/src/main/java/br/com/julio/layout/controller/TaskController.java
+++ layout/src/main/java/br/com/julio/layout/controller/TaskController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,14 +17,23 @@ public class TaskController {
     List<Task> tasks = new ArrayList<>();
 
     @GetMapping("/create")
-    public String home() {
-        return "create";
+    public ModelAndView home() {
+        ModelAndView mv = new ModelAndView("create");
+        mv.addObject("task", new Task());
+        return mv;
     }
 
     @PostMapping("/create")
     public String create(Task task){
-        Long id = tasks.size() + 1L;
-        tasks.add(new Task(id, task.getName(), task.getDate()));
+
+        if(task.getId() != null){
+            Task taskFind = tasks.stream().filter(taskItem -> task.getId().equals(taskItem.getId())).findFirst().get();
+            tasks.set(tasks.indexOf(taskFind), task);
+        } else {
+            Long id = tasks.size() + 1L;
+            tasks.add(new Task(id, task.getName(), task.getDate()));
+        }
+
         return "redirect:/list";    
     }
 
@@ -32,6 +42,14 @@ public class TaskController {
     public ModelAndView list(){
         ModelAndView mv = new ModelAndView("list");
         mv.addObject("tasks", tasks);
+        return mv;
+    }
+
+    @GetMapping("/edit/{id}")
+    public ModelAndView edit(@PathVariable("id") Long id){
+        ModelAndView mv = new ModelAndView("create");
+        Task taskFind = tasks.stream().filter(task -> id.equals(task.getId())).findFirst().get();
+        mv.addObject( "task", taskFind);
         return mv;
     }
 
